34e736faacce09eb3d0dfd2dbc5086a53619ab34
Korisnik <afankralj@gmail.com>
2026-02-03 11:52:22 +0100

    dodo temp i video

--- Afanov app.py
+++ Afanov app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    # Koristimo <h1> tag za velika, podebljana slova
-    return "<h1>Afanbrale napravio web stranicu :O</h1>"
+def pocetna():
+    # Ovo traži index.html unutar 'templates' foldera
+    return render_template('index.html')
+
+@app.route('/moj-video')
+def video_stranica():
+    # Ovo traži video.html unutar 'templates' foldera
+    return render_template('video.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
